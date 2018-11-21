00d766d9ca9f5af5c4993a692293b22b9102ad3d
Rafael Augusto <rareis@stefanini.com>
2018-11-21 19:20:34 -0200

    Chield

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import faker from 'faker';
 
 import ComentDetail from './CommentDetail';
+import ApprovalCard from './CommentDetail';
 
 const App = () => (
+  
     <div className="ui container comments">
-      <ComentDetail author="Sam" />
-      <ComentDetail author="Alex" />
-      <ComentDetail author="Jane" />
+    <ApprovalCard>
+      <ComentDetail author="Sam" timeAgo="Today at 4:45PM" contnet="Bem nice!" avatar={faker.image.avatar()} />
+    </ApprovalCard>
+
+    <ApprovalCard>
+      <ComentDetail author="Sam" timeAgo="Today at 4:45PM" contnet="Bem nice!" avatar={faker.image.avatar()} />
+    </ApprovalCard>
+
+    <ApprovalCard>
+    <ComentDetail author="Jane" timeAgo="Now" contnet="LOL" avatar={faker.image.avatar()} />
+    </ApprovalCard>
+     
     </div>
 );
 
---
+++ src/ApprovalCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ApprovalCard = (props) => {
+    console.log("Props", props)
+
+    return (
+        <div className="ui card">
+            <div className="content">{props.children}</div>
+            <div className="extra content">
+                <div className="ui tow buttons">
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Approve</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ApprovalCard;
